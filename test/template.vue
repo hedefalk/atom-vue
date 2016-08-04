2f19e1d51963e04fac66afb077e6f776ab54bd3e
Aidi Stan <aidistan@live.com>
2016-08-04 23:09:48 +0800

    Support text.slm (#31)

--- test/template.vue
+++ test/template.vue
@@ -45,3 +45,31 @@ div.class
     <path d="M3.835,5.607l-1.839,-1.839l-1.215,1.214l3.089,3.089l0.607,-0.643l5.304,-5.625l-1.25,-1.178l-4.696,4.982Z" style="fill:#7ed321;"></path>
   </svg>
 </template>
+
+<!--
+  Support text.slm
+  https://github.com/hedefalk/atom-vue/issues/31
+-->
+
+<template lang="slm">
+h1 Markup examples
+
+#content
+  p This example shows you how a basic Slm file looks.
+
+== content()
+
+- if this.items.length
+  table#items
+    - for item in this.items
+      tr
+        td.name = item.name
+        td.price = item.price
+- else
+  p No items found Please add some inventory.
+    Thank you!
+
+div id="footer"
+  == partial('footer')
+  | Copyright &copy; ${this.year} ${this.author}
+</template>
