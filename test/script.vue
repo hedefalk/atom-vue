0b24787770742c3f10d3f12b0897a56abbe6873e
Aidi Stan <aidistan@live.com>
2017-01-18 10:25:47 +0800

    Add a test for scripts

---
+++ test/script.vue
@@ -0,0 +1,8 @@
+<!--
+  Support language-todo correctly
+  https://github.com/hedefalk/atom-vue/issues/52
+-->
+
+<script>
+// NOTE: this 'NOTE' should be highlighted
+// NOTE: this 'NOTE' should be highlighted too </script>
