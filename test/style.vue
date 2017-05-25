d81ab8512b49ac41fb969c6d3723a5613252898d
Aidi Stan <aidistan@live.com>
2017-05-25 13:39:26 +0800

    Support SugarSS
    Fix #64

--- test/style.vue
+++ test/style.vue
@@ -72,3 +72,24 @@ body
 a.button
   border-radius: 5px
 </style>
+
+<!--
+  Support source.css.postcss.sugarss
+  https://github.com/hedefalk/atom-vue/issues/64
+-->
+
+<style lang="sugarss">
+h1, h2
+  font-weight: normal
+
+ul
+  list-style-type: none
+  padding: 0
+
+li
+  display: inline-block
+  margin: 0 10px
+
+a
+  color: #42b983
+</style>
