fec0a22c048323f027ea7bbcc9a8ff1110e228ed
confused-Techie <dev@lhbasics.com>
2023-08-01 19:58:25 -0700

    Finish building tests for `style.vue`

--- spec/grammars-spec.js
+++ spec/grammars-spec.js
@@ -315,6 +315,80 @@ a.button
 `
       );
 
+      expect(tokens[0]).toEqual({
+        value: "<",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "punctuation.definition.tag.html" ]
+      });
+
+      expect(tokens[1]).toEqual({
+        value: "style",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "entity.name.tag.style.html" ]
+      });
+
+      expect(tokens[2]).toEqual({
+        value: " ",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html" ]
+      });
+
+      expect(tokens[3]).toEqual({
+        value: "lang",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "entity.other.attribute-name.html" ]
+      });
+
+      expect(tokens[4]).toEqual({
+        value: "=",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html" ]
+      });
+
+      expect(tokens[5]).toEqual({
+        value: '"',
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "string.quoted.double.html", "punctuation.definition.string.begin.html" ]
+      });
+
+      expect(tokens[6]).toEqual({
+        value: "stylus",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "string.quoted.double.html" ]
+      });
+
+      expect(tokens[7]).toEqual({
+        value: '"',
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "string.quoted.double.html", "punctuation.definition.string.end.html" ]
+      });
+
+      expect(tokens[8]).toEqual({
+        value: ">",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "punctuation.definition.tag.html" ]
+      });
+
+      expect(tokens[9]).toEqual({
+        value: "\n" +
+          "border-radius()\n" +
+          "  -webkit-border-radius: arguments\n" +
+          "  -moz-border-radius: arguments\n" +
+          "  border-radius: arguments\n" +
+          "\n" +
+          "body\n" +
+          "  font: 12px Helvetica, Arial, sans-serif\n" +
+          "\n" +
+          "a.button\n" +
+          "  border-radius: 5px\n",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html" ]
+      });
+
+      expect(tokens[10]).toEqual({
+        value: "</",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "punctuation.definition.tag.html" ]
+      });
+
+      expect(tokens[11]).toEqual({
+        value: "style",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "entity.name.tag.style.html" ]
+      });
+
+      expect(tokens[12]).toEqual({
+        value: ">",
+        scopes: [ "text.html.vue", "source.css.stylus.embedded.html", "punctuation.definition.tag.html" ]
+      });
     });
 
     it("supports 'source.css.postcss.sugarss' injection", () => {
@@ -339,6 +413,83 @@ a
 `
       );
 
+      expect(tokens[0]).toEqual({
+        value: "<",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "punctuation.definition.tag.html" ]
+      });
+
+      expect(tokens[1]).toEqual({
+        value: "style",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "entity.name.tag.style.html" ]
+      });
+
+      expect(tokens[2]).toEqual({
+        value: " ",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html" ]
+      });
+
+      expect(tokens[3]).toEqual({
+        value: "lang",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "entity.other.attribute-name.html" ]
+      });
+
+      expect(tokens[4]).toEqual({
+        value: "=",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html" ]
+      });
+
+      expect(tokens[5]).toEqual({
+        value: '"',
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "string.quoted.double.html", "punctuation.definition.string.begin.html" ]
+      });
+
+      expect(tokens[6]).toEqual({
+        value: "sugarss",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "string.quoted.double.html" ]
+      });
+
+      expect(tokens[7]).toEqual({
+        value: '"',
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "string.quoted.double.html", "punctuation.definition.string.end.html" ]
+      });
+
+      expect(tokens[8]).toEqual({
+        value: ">",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "punctuation.definition.tag.html" ]
+      });
+
+      expect(tokens[9]).toEqual({
+        value: "\n" +
+          "h1, h2\n" +
+          "  font-weight: normal\n" +
+          "\n" +
+          "ul\n" +
+          "  list-style-type: none\n" +
+          "  padding: 0\n" +
+          "\n" +
+          "li\n" +
+          "  display: inline-block\n" +
+          "  margin: 0 10px\n" +
+          "\n" +
+          "a\n" +
+          "  color: #42b983\n",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html" ]
+      });
+
+      expect(tokens[10]).toEqual({
+        value: "</",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "punctuation.definition.tag.html" ]
+      });
+
+      expect(tokens[11]).toEqual({
+        value: "style",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "entity.name.tag.style.html" ]
+      });
+
+      expect(tokens[12]).toEqual({
+        value: ">",
+        scopes: [ "text.html.vue", "source.css.postcss.sugarss.embedded.html", "punctuation.definition.tag.html" ]
+      });
 
     });
 
